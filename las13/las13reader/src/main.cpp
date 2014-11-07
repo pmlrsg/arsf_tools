92a9b885146c08eaf720424cb16efa32e7630649
arsf <arsf-processing@pml.ac.uk>
2014-11-07 15:58:18 +0000

    changed file to come from command line but with no checking

--- las13/las13reader/src/main.cpp
+++ las13/las13reader/src/main.cpp
@@ -2,35 +2,46 @@
 #include <iostream>
 #include <fstream>
 
-int main(void)
+//This is a test program intended to be edited by you before compiling
+//There is no command line testing etc 
+//Note you need to pass a LAS1.3 file as first argument
+
+int main (int argc, char const* argv[])
 {
-   std::cout.precision(10);
-   //Las1_3_handler lasHandler("/users/rsg/mark1/scratch_space/EUFAR/testscripts/LDR-FW-FW10_01-2010-098-01.LAS");
-   Las1_3_handler lasHandler("/users/rsg/arsf/arsf_data/2014/flight_data/uk/RG12_09-2014_172_Wessex/delivery/RG12_09-172-lidar-20140923/flightlines/fw_laser/las1.3/LDR-FW-RG12_09-2014-172-01.LAS");
+   try
+   {
+      std::cout.precision(10);
+      std::string filename=argv[1];
+      Las1_3_handler lasHandler(filename);
 
-   PulseManager *p = lasHandler.GetPointsWithClassification(7);
-//   PulseManager *p = lasHandler.GetPointsInBounds(103000.0,102995.0,432915.0,432920.0);
-   std::cout << "the pulse manager has : " << p->getNumOfPulses() << " pulses\n";
+      PulseManager *p = lasHandler.GetPointsWithClassification(7);
+   //   PulseManager *p = lasHandler.GetPointsInBounds(103000.0,102995.0,432915.0,432920.0);
+      std::cout << "the pulse manager has : " << p->getNumOfPulses() << " pulses\n";
 
-//   Pulse* pulse=p->getPulse(0);
-//   if(pulse!=NULL)
-//   {
-//      std::cout<<pulse->originXYZ().size()<<std::endl;
-//      std::cout<<" "<<pulse->originXYZ()[0]<<" "<<pulse->originXYZ()[1]<<" "<<pulse->originXYZ()[2]<<std::endl; 
-//   }
+      Pulse* pulse=(*p)[0];
+      if(pulse!=NULL)
+      {
+         std::cout<<pulse->originXYZ().size()<<std::endl;
+         std::cout<<" "<<pulse->originXYZ()[0]<<" "<<pulse->originXYZ()[1]<<" "<<pulse->originXYZ()[2]<<std::endl; 
+      }
 
-//   PulseManager *p =NULL;
-//   int npulses=0;
-//   for(int loop=0;loop<20;loop++)
-//   {
-//      p=lasHandler.ReadLikeBook(1000000);
-//      if(p!=NULL)   
-//      {
-//         npulses=p->getNumOfPulses();
-//         std::cout <<"iteration "<<loop << "the pulse manager has : " << npulses << " pulses\n";
-//      }
-//   }
-   //p->printPulseInfo(0);  
-   lasHandler.DeletePulseManagers();
+   //   PulseManager *p =NULL;
+   //   int npulses=0;
+   //   for(int loop=0;loop<20;loop++)
+   //   {
+   //      p=lasHandler.ReadLikeBook(1000000);
+   //      if(p!=NULL)   
+   //      {
+   //         npulses=p->getNumOfPulses();
+   //         std::cout <<"iteration "<<loop << "the pulse manager has : " << npulses << " pulses\n";
+   //      }
+   //   }
+      //p->printPulseInfo(0);  
+      lasHandler.DeletePulseManagers();
+   }
+   catch(std::string e)
+   {
+      std::cout<<e<<std::endl;
+   }
    return 0;
 }
